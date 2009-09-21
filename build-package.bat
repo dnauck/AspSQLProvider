96fb847b00ebb990becce6a1ba6aecfa38992850
Daniel Nauck <d.nauck@nauck-it.de>
2009-09-21 23:56:17 +0200

    Set Configuration property to Release on package build

--- build-package.bat
+++ build-package.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
-Tools\NAnt\NAnt.exe package -buildfile:AspSQLProvider.build -D:CCNetLabel=0.0.0.0 -nologo -logfile:nant-build-package.log.txt %*
+Tools\NAnt\NAnt.exe package -buildfile:AspSQLProvider.build -D:build.configuration=Release -D:CCNetLabel=0.0.0.0 -nologo -logfile:nant-build-package.log.txt %*
 echo %time% %date%
 pause
--- build-package.sh
+++ build-package.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mono Tools/NAnt/NAnt.exe package -buildfile:AspSQLProvider.build -D:CCNetLabel=0.0.0.0 -nologo -logfile:nant-build-package.log
+mono Tools/NAnt/NAnt.exe package -buildfile:AspSQLProvider.build -D:build.configuration=Release -D:CCNetLabel=0.0.0.0 -nologo -logfile:nant-build-package.log
