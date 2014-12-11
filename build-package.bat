dd458c9a4fe25a76e9d1becffe2084c0f0a589d2
Daniel Nauck <d.nauck@nauck-it.de>
2014-12-11 19:00:12 +0100

    remove old build tooling

--- build-package.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-Tools\NAnt\NAnt.exe package -buildfile:AspSQLProvider.build -D:build.configuration=Release -D:CCNetLabel=0.0.0.0 -nologo -logfile:nant-build-package.log.txt %*
-echo %time% %date%
-pause
